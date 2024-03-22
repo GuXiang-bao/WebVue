5edbf60d27848b522368dbddaef5ac8a4a4daf3b
GuXiang <guxiang.bao.@qq.com>
2024-03-22 19:11:54 +0800

    配置了index.js启动文件

---
+++ serve/config/default.js
@@ -0,0 +1,7 @@
+const config = {
+
+    //启动的端口号
+    port : 3000,
+}
+
+module.exports = config;
---
+++ serve/routes/index.js
@@ -0,0 +1,10 @@
+
+
+
+module.exports = function (app) {
+    //test
+    app.get('/test',(req,res) => {
+        res.type('html');
+        res.render('test');
+    })
+}
